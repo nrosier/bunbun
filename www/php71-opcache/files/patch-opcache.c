5b989cc82210e3f01df1c319b6fc91dbb190a46c
Nick Rosier <github@bunbun.be>
2017-06-25 22:14:46 +0200

    fix opcache

---
+++ www/php71-opcache/files/patch-opcache.c
@@ -0,0 +1,14 @@
+--- a/ext/opcache/zend_file_cache.c
++++ b/ext/opcache/zend_file_cache.c
+@@ -24,9 +24,10 @@
+ 
+ #include "php.h"
+ 
++#include "ZendAccelerator.h"
++
+ #ifdef HAVE_OPCACHE_FILE_CACHE
+ 
+-#include "ZendAccelerator.h"
+ #include "zend_file_cache.h"
+ #include "zend_shared_alloc.h"
+ #include "zend_accelerator_util_funcs.h"

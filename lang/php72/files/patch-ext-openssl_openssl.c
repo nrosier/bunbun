d4e1f4d234ea3dfe4c7b1263560f69558bbd4b6f
Nick Rosier <github@bunbun.be>
2018-04-19 09:50:04 +0200

    New patch LibreSSL

--- lang/php72/files/patch-ext-openssl_openssl.c
+++ lang/php72/files/patch-ext-openssl_openssl.c
@@ -1,6 +1,15 @@
---- ext/openssl/openssl.c.orig	2018-02-27 16:33:08 UTC
-+++ ext/openssl/openssl.c
-@@ -564,7 +564,7 @@ ZEND_GET_MODULE(openssl)
+--- ext/openssl/openssl.c.orig	2018-04-04 14:26:34.583000000 +0000
++++ ext/openssl/openssl.c	2018-04-04 14:20:16.907000000 +0000
+@@ -73,7 +73,7 @@
+ #ifdef HAVE_OPENSSL_MD2_H
+ #define OPENSSL_ALGO_MD2	4
+ #endif
+-#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined (LIBRESSL_VERSION_NUMBER)
++#if OPENSSL_VERSION_NUMBER < 0x10100000L || (defined (LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
+ #define OPENSSL_ALGO_DSS1	5
+ #endif
+ #define OPENSSL_ALGO_SHA224 6
+@@ -560,7 +560,7 @@
  #endif
  
  /* {{{ OpenSSL compatibility functions and macros */
@@ -9,7 +18,7 @@
  #define EVP_PKEY_get0_RSA(_pkey) _pkey->pkey.rsa
  #define EVP_PKEY_get0_DH(_pkey) _pkey->pkey.dh
  #define EVP_PKEY_get0_DSA(_pkey) _pkey->pkey.dsa
-@@ -681,7 +681,7 @@ static const unsigned char *ASN1_STRING_
+@@ -677,7 +677,7 @@
  	return M_ASN1_STRING_data(asn1);
  }
  
@@ -18,25 +27,25 @@
  
  static int X509_get_signature_nid(const X509 *x)
  {
-@@ -1416,7 +1416,7 @@ PHP_MINIT_FUNCTION(openssl)
- 	le_x509 = zend_register_list_destructors_ex(php_openssl_x509_free, NULL, "OpenSSL X.509", module_number);
- 	le_csr = zend_register_list_destructors_ex(php_openssl_csr_free, NULL, "OpenSSL X.509 CSR", module_number);
- 
+@@ -1324,7 +1324,7 @@
+ 			mdtype = (EVP_MD *) EVP_md2();
+ 			break;
+ #endif
 -#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined (LIBRESSL_VERSION_NUMBER)
 +#if OPENSSL_VERSION_NUMBER < 0x10100000L || (defined (LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
- 	OPENSSL_config(NULL);
- 	SSL_library_init();
- 	OpenSSL_add_all_ciphers();
-@@ -1597,7 +1597,7 @@ PHP_MINFO_FUNCTION(openssl)
-  */
- PHP_MSHUTDOWN_FUNCTION(openssl)
- {
+ 		case OPENSSL_ALGO_DSS1:
+ 			mdtype = (EVP_MD *) EVP_dss1();
+ 			break;
+@@ -1450,7 +1450,7 @@
+ #ifdef HAVE_OPENSSL_MD2_H
+ 	REGISTER_LONG_CONSTANT("OPENSSL_ALGO_MD2", OPENSSL_ALGO_MD2, CONST_CS|CONST_PERSISTENT);
+ #endif
 -#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined (LIBRESSL_VERSION_NUMBER)
 +#if OPENSSL_VERSION_NUMBER < 0x10100000L || (defined (LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
- 	EVP_cleanup();
- 
- 	/* prevent accessing locking callback from unloaded extension */
-@@ -3642,7 +3642,7 @@ PHP_FUNCTION(openssl_csr_get_public_key)
+ 	REGISTER_LONG_CONSTANT("OPENSSL_ALGO_DSS1", OPENSSL_ALGO_DSS1, CONST_CS|CONST_PERSISTENT);
+ #endif
+ 	REGISTER_LONG_CONSTANT("OPENSSL_ALGO_SHA224", OPENSSL_ALGO_SHA224, CONST_CS|CONST_PERSISTENT);
+@@ -3620,7 +3620,7 @@
  		RETURN_FALSE;
  	}
  
@@ -45,7 +54,7 @@
  	/* Due to changes in OpenSSL 1.1 related to locking when decoding CSR,
  	 * the pub key is not changed after assigning. It means if we pass
  	 * a private key, it will be returned including the private part.
-@@ -3653,7 +3653,7 @@ PHP_FUNCTION(openssl_csr_get_public_key)
+@@ -3631,7 +3631,7 @@
  	/* Retrieve the public key from the CSR */
  	tpubkey = X509_REQ_get_pubkey(csr);
  

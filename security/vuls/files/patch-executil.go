a2d60c737a0f0954abbd5e1021e92e120d3974d1
Nick Rosier <nick@bunbun.be>
2019-03-20 22:32:20 +0100

    Fix vuls patches

--- security/vuls/files/patch-executil.go
+++ security/vuls/files/patch-executil.go
@@ -1,5 +1,5 @@
---- executil.go.orig	2019-03-20 22:22:55.864055000 +0100
-+++ executil.go	2019-03-20 22:23:29.957560000 +0100
+--- scan/executil.go.orig	2019-03-20 22:22:55.864055000 +0100
++++ scan/executil.go	2019-03-20 22:23:29.957560000 +0100
 @@ -385,6 +385,8 @@
  			if c.User != "root" {
  				cmd = fmt.Sprintf("sudo -S %s", cmd)
